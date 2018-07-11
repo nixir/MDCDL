6677e3d560e0563a78d0cb6446acd97548e7fae5
sngy <satoshi.nagayama.jp@ieee.org>
2018-07-11 23:34:37 +0900

    refactorize mlcsc.jl

--- src/mlcsc.jl
+++ src/mlcsc.jl
@@ -1,47 +1,18 @@
-function analyze(mlcsc::MDCDL.MultiLayerCsc{TC,D}, x::Array{TX,D}; isAllCoefs::Bool=false) where {TC,TX,D}
-    γ = Vector(mlcsc.nLayers+1)
-    γ[1] = x
-    for l = 1:mlcsc.nLayers
-        dic = mlcsc.dictionaries[l]
-
-        γ[l+1] = analyze(dic, γ[l]; outputMode=:augumented)
-    end
-
-    if isAllCoefs
-        γ
-    else
-        γ[mlcsc.nLayers+1]
+function analyze(mlcsc::MDCDL.MultiLayerCsc{TC,D}, x::Array{TX,D}) where {TC,TX,D}
+    foldl(x, mlcsc.dictionaries) do tx, dic
+        analyze(dic, tx; outputMode=:augumented)
     end
 end
 
 function adjoint_synthesize(mlcsc::MDCDL.MultiLayerCsc{TC,D}, x::Array{TX,D}; isAllCoefs::Bool=false) where {TC,TX,D}
-    γ = Vector(mlcsc.nLayers+1)
-    γ[1] = x
-    for l = 1:mlcsc.nLayers
-        dic = mlcsc.dictionaries[l]
-
-        γ[l+1] = adjoint_synthesize(dic, γ[l]; outputMode=:augumented)
-    end
-
-    if isAllCoefs
-        γ
-    else
-        γ[mlcsc.nLayers+1]
+    foldl(x, mlcsc.dictionaries) do tx, dic
+        adjoint_synthesize(dic, tx; outputMode=:augumented)
     end
 end
 
 function synthesize(mlcsc::MDCDL.MultiLayerCsc, y::Array; isAllCoefs::Bool=false)
-    γ = Vector(mlcsc.nLayers+1)
-    γ[mlcsc.nLayers+1] = y
-    for l = mlcsc.nLayers:-1:1
-        dic = mlcsc.dictionaries[l]
-        γ[l] = synthesize(dic, γ[l+1])
-    end
-
-    if isAllCoefs
-        γ
-    else
-        γ[1]
+    foldr(y, mlcsc.dictionaries) do dic, ty
+        synthesize(dic, ty)
     end
 end
 
@@ -101,7 +72,7 @@ function mlfista(mlcsc::MDCDL.MultiLayerCsc, x, λs::Vector{T}; maxIterations::I
         tk = (1 + sqrt(1+4*tkprev)) / 2
 
         z = γ[L+1] + (tkprev - 1)/tk * (γ[L+1] - glp)
-        if viewStatuså
+        if viewStatus
             println("Iteration $k finished.")
         end
     end
