a90474caaba2e8f5d465c1a9c7f5fd17f1516467
sngy <satoshi.nagayama.jp@ieee.org>
2018-07-17 21:43:49 +0900

    update examples

--- examples/main_image_restoration.jl
+++ examples/main_image_restoration.jl
@@ -4,20 +4,23 @@ using ImageFiltering
 
 ### configurations ###
 dirNsolt = joinpath(Pkg.dir(),"MDCDL","examples","design","sample.json")
-σ_noise = 3e-2
+σ_noise = 1e-2
 λ = 1e-2
 lv = 1 # tree level of NSOLT
 ######################
 
 ### setup observed image
-orgImg = testimage("cameraman")
-u = Array{Float64}(imresize(orgImg,(256,256)))
+# orgImg = testimage("cameraman")
+# u = Array{Float64}(imresize(orgImg,(256,256)))
+orgImg = testimage("lena")
+u = Array{RGB{Float64}}(orgImg)
 
 # AWGN
-w = σ_noise * randn(size(u))
+# w = σ_noise * randn(size(u))
+w = mapc.((v)->σ_noise * randn(), u)
 
 # Degradation (pixel loss)
-P = map((v)-> v > 0.1 ? 1 : 0, rand(size(u)))
+P = map((v)-> v > 0.01 ? 1 : 0, rand(size(u)))
 
 # Observation
 x = P.*u + w
@@ -33,19 +36,19 @@ println(" - Polyphase Order = $(nsolt.polyphaseOrder)")
 # analysis/synthesis filter set
 mlpfb = Multiscale(ParallelFB(nsolt), lv)
 
-y0 = analyze(mlpfb, x)
+y0 = analyze(mlpfb, x; outputMode = :vector)
 
 gradOfLossFcn = (ty) -> begin
-    - analyze(mlpfb, P.*(x - synthesize(mlpfb, ty)))
+    - analyze(mlpfb, P.*(x - synthesize(mlpfb, ty, size(x))); outputMode = :vector)
 end
 proxFcn = (ty, η) -> MDCDL.softshrink(ty, λ*η)
 viewFcn = (itrs, ty, err) -> begin
     println("# Iterations=$itrs, error=$err")
 end
 
-hy = MDCDL.fista(gradOfLossFcn, proxFcn, 1.0, y0; maxIterations=200, viewFunction=viewFcn, absTol=eps())
+hy = MDCDL.fista(gradOfLossFcn, proxFcn, 1.0, y0; maxIterations=400, viewFunction=viewFcn, absTol=eps())
 
-ru = Array{Gray{Float64}}(synthesize(mlpfb, hy))
+ru = synthesize(mlpfb, hy, size(x))
 
 errx = vecnorm(ru - u)
 
