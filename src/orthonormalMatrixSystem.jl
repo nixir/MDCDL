b27d73d23b4ebff32342ad473b308e3ad84a1899
sngy <satoshi.nagayama.jp@ieee.org>
2018-07-30 17:46:57 +0900

    create method of calculate a gradient of an orthonormal matrix

--- src/orthonormalMatrixSystem.jl
+++ src/orthonormalMatrixSystem.jl
@@ -22,17 +22,17 @@ function mat2rotations(mtx::Matrix{T}) where T <: Real
     (res, round.(diag(mtx)))
 end
 
-function rotations2mat(angs::Array{T}, sig::Array{T}) where T <: Real
+function rotations2mat(angs::Array{TA}, sig::Array{TS}) where {TA<:Real,TS<:Number}
     L = length(angs)
     P = round(Integer, (1 + sqrt(1+8*L)) / 2)
-    mtx = eye(T,P)
+    mtx = eye(TA,P)
 
     nr = 1
     for idx1 = 1:P-1, idx2 = (idx1+1):P
         c = cos(angs[nr])
         s = sin(angs[nr])
 
-        rtm = eye(T,P)
+        rtm = eye(TA,P)
         rtm[idx1, idx1] =  c
         rtm[idx1, idx2] = -s
         rtm[idx2, idx1] =  s
@@ -43,3 +43,25 @@ function rotations2mat(angs::Array{T}, sig::Array{T}) where T <: Real
     end
     mtx * diagm(sig)
 end
+
+# ∂(x'*A(θ)*y)/∂θ
+function scalarGradOfOrthonormalMatrix(x::AbstractArray{TV,D}, y::AbstractArray{TV,D}, angs::Array{TA}, sig::Array{TS}) where {D,TV,TA<:Real, TS<:Number}
+    L = length(angs)
+    P = round(Integer, (1 + sqrt(1+8*L)) / 2)
+
+    output = zeros(TA, L)
+    nr = 1
+    for idx1 = 1:P-1, idx2 = (idx1+1):P
+        c, s = cos(angs[nr]), sin(angs[nr])
+        sub1 = (idx1,fill(:,D-1)...)
+        sub2 = (idx2,fill(:,D-1)...)
+
+        ty1 = sig[idx1]*(-s * y[sub1...] + c * y[sub2...])
+        ty2 = sig[idx2]*(-c * y[sub1...] - s * y[sub2...])
+        output[nr] = dot(x[sub1...], ty1) + dot(x[sub2...], ty2)
+        nr += 1
+    end
+    output
+end
+
+scalarGradOfOrthonormalMatrix(x::AbstractVector, y::AbstractVector, A::AbstractMatrix) = scalarGradOfOrthonormalMatrix(x, y, mat2rotasions...)
