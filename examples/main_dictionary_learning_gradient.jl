5af41b039d9647e2f635ae60b753db4ece217f0a
sngy <satoshi.nagayama.jp@ieee.org>
2018-08-01 23:25:43 +0900

    update examples

--- examples/main_dictionary_learning_gradient.jl
+++ examples/main_dictionary_learning_gradient.jl
@@ -28,7 +28,7 @@ nEpoch = 10
 
 nsolt = Rnsolt(dt, df, ord, nch)
 include(joinpath(Pkg.dir(),"MDCDL","test","randomInit.jl"))
-randomInit!(nsolt)
+# randomInit!(nsolt)
 
 orgImg = Array{dt}(testimage("cameraman"))
 trainingIds = [ (colon.(1,szSubData) .+ rand.(colon.(0,size(orgImg) .- szSubData))) for nsd in 1:nSubData ]
@@ -39,21 +39,11 @@ sparsity = fld(length(y0), 8)
 angs0, mus0 = getAngleParameters(nsolt)
 angs0s = angs0[nch[1]:end]
 
-# opt = Opt(:LN_COBYLA, length(angs0s))
-opt = Opt(:LD_CCSAQ, length(angs0s))
-# lower_bounds!(opt, -1*pi*ones(size(angs0s)))
-# upper_bounds!(opt,  1*pi*ones(size(angs0s)))
-xtol_rel!(opt,1e-4)
-maxeval!(opt,2000)
-
-# inequality_constraint!(opt, (x,g) -> myconstraint(x,g,2,0), 1e-8)
-# inequality_constraint!(opt, (x,g) -> myconstraint(x,g,-1,1), 1e-8)
-
-# (minf, minx, ret) = optimize(opt, [1.234, 5.678])
 y = y0
-for idx = 1:nEpoch, subx in trainingIds
+for epoch = 1:nEpoch, nd in 1:length(trainingIds)
+    subx = trainingIds[nd]
     x = orgImg[subx...]
-    hy = MDCDL.iht(nsolt, x, y, sparsity; maxIterations=500, viewStatus=true, lt=(lhs,rhs)->isless(norm(lhs), norm(rhs)))
+    hy = MDCDL.iht(nsolt, x, y, sparsity; maxIterations=100, viewStatus=false, lt=(lhs,rhs)->isless(norm(lhs), norm(rhs)))
     cnt = 0
     objfunc = (angs::Vector, grad::Vector) -> begin
         global cnt
@@ -70,13 +60,7 @@ for idx = 1:nEpoch, subx in trainingIds
 
         nBlocks = mdarray2polyphase(x - rx, df).nBlocks
         et = mdarray2polyphase(x - rx, df).data
-
-        yt = begin
-            yaug = reshape(hy, fld.(size(x),df)..., sum(nch))
-            mdarray2polyphase(yaug).data
-        end
-
-        fet = tfb.matrixC * flipdim(et, 1)
+        yt = mdarray2polyphase(rx, df).data
 
         M = prod(df)
         cM, fM = cld(M,2), fld(M,2)
@@ -86,83 +70,109 @@ for idx = 1:nEpoch, subx in trainingIds
         P[1:nch[1],1:cM] = Pu
         P[nch[1]+1:end,cM+1:end] = Pl
 
-        pet = P*fet
+        pet = P * nsolt.matrixC * et
+        pyt = P * nsolt.matrixC * yt
         # rand!(pet)
 
         # println(η*max(maximum(gdw), maximum(gdu)))
-        W0 = copy(tfb.initMatrices[1])
-        U0 = copy(tfb.initMatrices[2])
+        W0 = copy(nsolt.initMatrices[1])
+        U0 = copy(nsolt.initMatrices[2])
+
+        pyt[1:nch[1],:] = W0 * pyt[1:nch[1],:]
+        pyt[(nch[1]+1):end,:] = U0 * pyt[(nch[1]+1):end,:]
 
         angsw, musw = MDCDL.mat2rotations(W0)
         angsu, musu = MDCDL.mat2rotations(U0)
 
-        gdw = -MDCDL.scalarGradOfOrthonormalMatrix(yt[1:nch[1],:], pet[1:nch[1],:], angsw, musw)
-        gdu = -MDCDL.scalarGradOfOrthonormalMatrix(yt[nch[1]+1:end,:], pet[nch[1]+1:end,:], angsu, musu)
+        gdw = -MDCDL.scalarGradOfOrthonormalMatrix(pyt[1:nch[1],:], pet[1:nch[1],:], angsw, musw)
+        gdu = -MDCDL.scalarGradOfOrthonormalMatrix(pyt[nch[1]+1:end,:], pet[nch[1]+1:end,:], angsu, musu)
 
-        # tfb.initMatrices[1] .= MDCDL.rotations2mat(angsw - η*gdw, musw)
-        # tfb.initMatrices[2] .= MDCDL.rotations2mat(angsu - η*gdu, musu)
+        # nsolt.initMatrices[1] .= MDCDL.rotations2mat(angsw - η*gdw, musw)
+        # nsolt.initMatrices[2] .= MDCDL.rotations2mat(angsu - η*gdu, musu)
 
         pet[1:nch[1],:] = W0*pet[1:nch[1],:]
         pet[nch[1]+1:end,:] = U0*pet[nch[1]+1:end,:]
 
-        hP = tfb.nChannels[1]
+        hP = nsolt.nChannels[1]
 
         L = fld(nch[2]*(nch[2]-1),2)
         gdudk = [ fill(zeros(dt,L), o) for o in ord ]
         for d = 1:D
             nShift = fld(size(pet,2), nBlocks[1])
             # pvx = permutedims(pvx)
-            pvx = MDCDL.permutedims(PolyphaseVector(pet, nBlocks))
-            typeof(pvx)
-            pet, nBlocks = pvx.data, pvx.nBlocks
+            pvxe = MDCDL.permutedims(PolyphaseVector(pet, nBlocks))
+            pet = pvxe.data
+            pvy  = MDCDL.permutedims(PolyphaseVector(pyt, nBlocks))
+            pyt, nBlocks = pvy.data, pvy.nBlocks
             # submatrices
-            xu = view(pet, 1:hP, :)
-            xl = view(pet, (1:hP)+hP, :)
+            xeu = view(pet, 1:hP, :)
+            xel = view(pet, (1:hP)+hP, :)
+            xyu = view(pyt, 1:hP, :)
+            xyl = view(pyt, (1:hP)+hP, :)
+            for k = 1:nsolt.polyphaseOrder[d]
+                teu, tel = (xeu + xel, xeu - xel) ./ sqrt(2)
+                xeu .= teu; xel .= tel
 
-            for k = 1:tfb.polyphaseOrder[d]
-                tu, tl = (xu + xl, xu - xl) ./ sqrt(2)
-                xu .= tu; xl .= tl
+                tyu, tyl = (xyu + xyl, xyu - xyl) ./ sqrt(2)
+                xyu .= tyu; xyl .= tyl
 
                 if isodd(k)
-                    xl .= circshift(xl, (0, nShift))
+                    xel .= circshift(xel, (0, nShift))
+                    xyl .= circshift(xyl, (0, nShift))
                 else
-                    xu .= circshift(xu, (0, -nShift))
+                    xeu .= circshift(xeu, (0, -nShift))
+                    xyu .= circshift(xyu, (0, -nShift))
                 end
-                tu, tl = (xu + xl, xu - xl) ./ sqrt(2)
-                xu .= tu; xl .= tl
 
-                txl = tfb.propMatrices[d][k] * xl
+                teu, tel = (xeu + xel, xeu - xel) ./ sqrt(2)
+                xeu .= teu; xel .= tel
 
-                angsu, musu = MDCDL.mat2rotations(tfb.propMatrices[d][k])
-                hoge = -MDCDL.scalarGradOfOrthonormalMatrix(yt[nch[1]+1:end,:], xl, angsu, musu)
-                gdudk[d][k]  .= hoge /vecnorm(x)
-                # tfb.propMatrices[d][k] .= MDCDL.rotations2mat(angsu - η*gdudk[d][k], musu)
-                # println(gdu)
+                tyu, tyl = (xyu + xyl, xyu - xyl) ./ sqrt(2)
+                xyu .= tyu; xyl .= tyl
 
-                xl .= txl
+                # txl = nsolt.propMatrices[d][k] * xl
+                Uk = nsolt.propMatrices[d][k]
+                xyl .= Uk * xyl
+
+                angsu, musu = MDCDL.mat2rotations(nsolt.propMatrices[d][k])
+                gdudk[d][k] = -MDCDL.scalarGradOfOrthonormalMatrix(xyl, xel, angsu, musu)
+                # nsolt.propMatrices[d][k] .= MDCDL.rotations2mat(angsu - η*gdu, musu)
+
+                xel .= Uk * xel
             end
         end
 
-        grad .= vcat(gdw[nch[1]:end], gdu, vcat(vcat.(gdudk...)...))
-        # println(typeof(vcat(vcat.(gdudk)...)))
 
-        # println(size(hoge))
-        # println(size(grad))
+        grad .= vcat(gdw[nch[1]:end], gdu, vcat(vcat.(gdudk...)...))*vecnorm(x)^(-2)
+
         setAngleParameters!(nsolt, angsvm1, mus0)
         dist = x .- synthesize(nsolt, hy, size(x))
         cst = vecnorm(dist)^2
 
         println("f_$(cnt):\t cost = $(cst),\t |grad| = $(vecnorm(grad))")
-        sleep(0.2)
+        # sleep(0.2)
         cst
     end
-    min_objective!(opt, objfunc)
-    (minf, minx, ret) = optimize(opt, angs0s)
+
+    # opt = Opt(:LN_COBYLA, length(angs0s))
+    lopt = Opt(:LD_MMA, length(angs0s))
+    maxeval!(lopt,100)
+    min_objective!(lopt, objfunc)
+    (minf, minx, ret) = optimize(lopt, angs0s)
+
+    ##### global optimization #####
+    # gopt = Opt(:GD_MLSL, length(angs0s))
+    # lower_bounds!(gopt, -1*pi*ones(size(angs0s)))
+    # upper_bounds!(gopt,  1*pi*ones(size(angs0s)))
+    # local_optimizer!(gopt, lopt)
+    # min_objective!(gopt, objfunc)
+    # (minf, minx, ret) = optimize(gopt, angs0s)
+    #################
 
     minxt = vcat(zeros(dt, sum(nch)-1), minx);
     setAngleParameters!(nsolt, minxt, mus0)
     y = analyze(nsolt, x; outputMode=:vector)
-    println("Iterations $idx finished.")
+    println("Epoch: $epoch, No.: $nd, cost = $(minf)")
 end
 
 atmimshow(nsolt)
