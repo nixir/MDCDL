a073dd3e10fe4becff5392db0b90f3199a404ae5
sngy <satoshi.nagayama.jp@ieee.org>
2018-09-12 21:31:36 +0900

    update

--- examples/main_dictionary_learning.jl
+++ examples/main_dictionary_learning.jl
@@ -2,32 +2,61 @@ using MDCDL
 using LinearAlgebra
 using TestImages
 
-Nsolt = Cnsolt
+using Base.Filesystem
+using Dates
+
+########## Configurations #########
+# choose NSOLT type: (Rnsolt | Cnsolt)
+Nsolt = Rnsolt
+# TP := eltype(Nsolt) (<:AbstractFloat)
 TP = Float64
+# decimation factor: (<:NTuple{D,Int} where D is #dims)
 df = (2,2)
+# polyphase order: (<:NTuple{D,Int} where D)
 ord = (4,4)
+# number of channels: (<:Union{Integer,Tuple{Int,Int}} for Rnsolt)
+#                     (<:Integer for Cnsolt)
 nch = 8
 
+# size of minibatches (<:NTuple{D,Int})
 szx = (16,16)
+# number of minibatches (<:Integer)
 nSubData = 32
 
+# path of log files (do nothing if isa(logdir, Nothing))
+# tm = Dates.format(now(), "yyyy_mm_dd_SS_sss")
+# logdir = joinpath(@__DIR__, "results", tm)
+logdir = nothing
+
+# options for sparse coding
+sc_options = ( iterations = 1000, sparsity = 0.5, filter_domain=:convolution)
+# options for dictionary update
+du_options = ( iterations = 1, stepsize = 1e-3,)
+
+# general options of dictionary learning
+options = ( epochs  = 100,
+            verbose = :standard,
+            sc_options = sc_options,
+            du_options = du_options,
+            logdir = logdir,)
+####################################
+
+logdir != nothing && !isdir(logdir) && mkpath(logdir)
+
+# original image
 orgImg = TP.(testimage("cameraman"))
+
+# generate minibatches of orgImage
 trainingIds = map(1:nSubData) do nsd
     pos = rand.(UnitRange.(0,size(orgImg) .- szx))
     UnitRange.(1 .+ pos, szx .+ pos)
 end
-
 trainingSet = map(idx -> orgImg[idx...], trainingIds)
 
+# create NSOLT instance
 nsolt = Nsolt(TP, df, ord, nch)
-MDCDL.rand!(nsolt, isPropMat = false, isSymmetry = false)
+# set initial matrices as random orthonormal ones.
+MDCDL.rand!(nsolt, isPropMat = false, isPropAng = false, isSymmetry = false)
 
-sc_options = ( iterations = 400, sparsity = 0.4, filter_domain=:convolution)
-du_options = ( iterations = 1, stepsize = 1e-3,)
-
-options = ( epochs  = 100,
-            verbose = :specified,
-            sc_options = sc_options,
-            du_options = du_options,)
-
+# dictionary learning
 MDCDL.train!(nsolt, trainingSet; options...)
--- src/dictionaryLearning.jl
+++ src/dictionaryLearning.jl
@@ -1,26 +1,34 @@
 using ForwardDiff
+using Base.Filesystem
+using Statistics
+using Dates
 
-function train!(nsolt::CB, trainingSet::AbstractArray; epochs::Integer=1, verbose::Union{Integer,Symbol}=1, sc_options=(), du_options=()) where {CB<:AbstractNsolt}
+function train!(nsolt::CB, trainingSet::AbstractArray; epochs::Integer=1, verbose::Union{Integer,Symbol}=1, logdir=Union{Nothing,AbstractString}=nothing, sc_options=(), du_options=()) where {CB<:AbstractNsolt}
     dict_vlevels = Dict(:none => 0, :standard => 1, :specified => 2, :loquacious => 3)
     vlevel = if verbose isa Integer; verbose else dict_vlevels[verbose] end
 
     θ, μ = getAngleParameters(nsolt)
     for itr = 1:epochs
         K = length(trainingSet)
-        lossvs = fill(Inf, K)
+        loss_sps = fill(Inf, K)
+        loss_dus = fill(Inf, K)
         for k = 1:K
             x = trainingSet[k]
+
+            hy, loss_sps[k] = stepSparseCoding(nsolt, x; sc_options...)
+            θ, μ, loss_dus[k] = updateDictionary(nsolt, x, hy, θ, μ; du_options...)
+
+            vlevel >= 2 && println("epoch #$itr, data #$k: loss(Sparse coding) = $(loss_sps[k]), loss(Dic. update) = $(loss_dus[k]).")
+
             setAngleParameters!(nsolt, θ, μ)
-            hy, loss_sp = stepSparseCoding(nsolt, x; sc_options...)
-
-            θ, μ, loss_du = updateDictionary(nsolt, x, hy, θ, μ; du_options...)
-            lossvs[k] = loss_du
-
-            vlevel >= 2 && println("epoch #$itr, data #$k: loss(Sparse coding) = $loss_sp, loss(Dic. update) = $loss_du.")
         end
         if vlevel >= 1
-            println("--- epoch #$itr, total loss = $(sum(lossvs))")
+            println("--- epoch #$itr, total sum(loss) = $(sum(loss_dus)), var(loss) = $(var(loss_dus))")
         end
+        log_params = [  "time" => Dates.format(now(), "yyy_mm_dd_SS_sss"),
+                        "loss_sparse_coding" => loss_sps,
+                        "loss_dictionary_update" => loss_dus ]
+        savelogs(logdir, nsolt, itr, log_params...)
     end
     vlevel >= 1 && println("training finished.")
     return setAngleParameters!(nsolt, θ, μ)
@@ -60,4 +68,17 @@ function updateDictionary(nsolt::NS, x::AbstractArray, hy::AbstractArray, θ::Ab
     end
     loss_opt = lossfcn(θopt)
     return (θopt, μ, loss_opt,)
+end
+
+savelogs(::Nothing, args...; kwargs...) = nothing
+function savelogs(dirname::AbstractString, nsolt::AbstractNsolt, epoch::Integer, params::Pair{String}...)
+    filename_logs = joinpath(dirname, "log")
+    filename_nsolt = joinpath(dirname, "nsolt.json")
+
+    strparams = map(prm->" $(prm.first) = $(prm.second),", params)
+    strlogs = string("epoch $epoch:", strparams...)
+    open(filename_logs, append=true) do io
+        println(io, strlogs)
+    end
+    save(nsolt, filename_nsolt)
 end
