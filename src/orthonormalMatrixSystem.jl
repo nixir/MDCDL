17e8378bca4e32d259520c481f5e17796d381a8a
sngy <satoshi.nagayama.jp@ieee.org>
2018-09-19 18:22:13 +0900

    refactorized

--- src/orthonormalMatrixSystem.jl
+++ src/orthonormalMatrixSystem.jl
@@ -1,3 +1,5 @@
+using SparseArrays
+
 function mat2rotations(mtx::AbstractMatrix{T}) where T <: Real
     P = size(mtx, 1)
 
@@ -5,13 +7,14 @@ function mat2rotations(mtx::AbstractMatrix{T}) where T <: Real
     ids = [ (idx1, idx2) for idx1 = 1:P-1 for idx2 = (idx1+1):P ]
 
     mtx = Array(mtx)
+    R = similar(mtx)
     for nr in 1:length(ids)
         a = givens(mtx, ids[nr][1], ids[nr][2], ids[nr][1])
         g = a[1]
 
         res[nr] = atan(g.s, g.c)
 
-        R = Matrix{T}(I,P,P)
+        R .= Matrix(I,P,P)
         R[g.i1, g.i1] =  g.c
         R[g.i1, g.i2] =  g.s
         R[g.i2, g.i1] = -g.s
@@ -27,13 +30,14 @@ end
 
 function rotations2mat(θs::AbstractArray{TA}, sig::AbstractArray{TS}, P::Integer) where {TA<:Real,TS<:Number}
     mtx = Matrix{TA}(I,P,P)
+    R = similar(mtx)
 
     ids = [ (idx1, idx2) for idx1 = 1:P-1 for idx2 = (idx1+1):P ]
     for nr in 1:length(ids)
         c, s = cos(θs[nr]), sin(θs[nr])
         idx1, idx2 = ids[nr][1], ids[nr][2]
 
-        R = Matrix{TA}(I,P,P)
+        R .= Matrix(I,P,P)
         R[idx1, idx1] =  c
         R[idx1, idx2] = -s
         R[idx2, idx1] =  s
